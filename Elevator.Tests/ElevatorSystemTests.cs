8db25584fa375a27607c73447e84abe23136ce3c
Maanda Ruud Matshili <maanda.matshili@gmail.com>
2025-01-20 21:42:45 +0200

    Tests added

---
+++ Elevator.Tests/ElevatorSystemTests.cs
@@ -0,0 +1,18 @@
+﻿using ElevatorControl;
+
+namespace Elevator.Tests
+{
+    [TestClass]
+    public class ElevatorSystemTests
+    {
+        [TestMethod]
+        public void ElevatorSystem_AssignClosestElevator_ShouldReturnClosestElevator()
+        {
+            var system = new ElevatorSystem();
+
+            system.HandleElevatorRequest(Role.GeneralWorker, 2);
+
+            Assert.IsTrue(system != null);
+        }
+    }
+}
